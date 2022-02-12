d76f72225d635f578c044a920823b9ba0e312bd2
roger <perezmenarogelio@gmail.com>
2022-02-11 18:13:37 -0600

    animations and dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import scrollreveal from "scrollreveal"
 import ScrollToTop from "./components/ScrollToTop"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
@@ -12,10 +13,50 @@ import Footer from "./components/Footer"
 import "./scss/index.scss"
 
 export default function App() {
+  const [theme, setTheme] = useState("dark")
+  const changeTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark")
+  }
+
+  useEffect(() => {
+    const registerAnimations = () => {
+      const sr = scrollreveal({
+        origin: "bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false,
+      })
+      sr.reveal(
+        `
+        nav,
+        .home,
+        .free,
+        .clients,
+        .super-rare,
+        .releases,
+        .like,
+        .signup,
+        footer
+    `,
+        {
+          interval: 500,
+        }
+      )
+    }
+    registerAnimations()
+  }, [])
+
+  window.setTimeout(() => {
+    const home = document.getElementsByClassName("home")
+    home[0].style.transform = "none"
+    const nav = document.getElementsByTagName("nav")
+    nav[0].style.transform = "none"
+  }, 1500)
+
   return (
-    <div className="app-container">
+    <div className="app-container" data-theme={theme}>
       <ScrollToTop />
-      <Navbar />
+      <Navbar changeTheme={changeTheme} currentTheme={theme} />
       <Home />
       <Free />
       <Clients />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { ImSun } from "react-icons/im"
 import { BsFillMoonFill } from "react-icons/bs"
 import logo from "../assets/logo.png"
 
-export default function Navbar() {
+export default function Navbar({ changeTheme, currentTheme }) {
   const [navState, setNavState] = useState(false)
   return (
     <nav>
@@ -15,7 +15,13 @@ export default function Navbar() {
         </div>
         <div className="toggle-container">
           <div className="toggle"></div>
-          <div className="mode"></div>
+          <div className="mode">
+            {currentTheme === "dark" ? (
+              <ImSun className="light" />
+            ) : (
+              <BsFillMoonFill className="dark" />
+            )}
+          </div>
         </div>
       </div>
       <div className="links-container">
@@ -31,6 +37,13 @@ export default function Navbar() {
           </li>
           <li>
             <a href="#">Sing up</a>
+          </li>
+          <li onClick={changeTheme}>
+            {currentTheme === "dark" ? (
+              <ImSun className="light" />
+            ) : (
+              <BsFillMoonFill className="dark" />
+            )}
           </li>
         </ul>
       </div>
